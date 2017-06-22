40c4d25ebe40dbd8432b9581ed3e3640f0fee928
Isaac Hardy <isaac@ihardy.co>
2017-06-22 18:20:17 -0400

    updated through classwork

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const parseurl = require("parse_url");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const validator = require("express-validator");
@@ -20,12 +19,16 @@ app.use(validator());
 
 app.use(session({
   secret: 'asdfsdafasafsdf',
-  resave: true,
-  saveUninitialized: true
+  resave: false, // What does this do?
+  saveUninitialized: true // What does this do?
 }));
 
+let messages = [];
+
+let users = [{username: "isaachardy", password: "password"}];
+
 app.get("/", function(req, res) {
-  if (req.session.email) {
+  if (req.session.username) {
     res.redirect("/user");
   } else {
     res.render("index");
@@ -37,20 +40,42 @@ app.get("/login", function(req, res) {
 });
 
 app.post("/login", function(req, res) {
-  req.session.email = req.body.email;
+  let loggedUser;
+  messages = [];
 
-  res.redirect("/user");
+  users.forEach(function(user) {
+    if (user.username === req.body.username) {
+      loggedUser = user;
+    }
+  });
 
+  req.checkBody("username", "Please Enter a valid username.").notEmpty().isLength({min: 8, max: 20});
+  req.checkBody("password", "Please Enter a Password.").notEmpty();
+  req.checkBody("password", "Invalid password and username combination.").equals(loggedUser.password);
+
+  let errors = req.validationErrors();
+
+  if (errors) {
+    errors.forEach(function(error) {
+      messages.push(error.msg);
+    });
+    res.render("login", {errors: messages});
+  } else {
+    req.session.cookie.secure = true;
+    req.session.username = req.body.username;
+
+    res.redirect("/user");
+  }
 });
 
 app.get("/user", function(req, res) {
-  if (req.session.email) {
-    if (req.session.viewCount >= 1) {
+  if (req.session.username) {
+    if (req.session.viewCount && req.session.viewCount >= 1) {
       req.session.viewCount++;
     } else {
       req.session.viewCount = 1;
     }
-    res.render("user", {email: req.session.email, viewCount: req.session.viewCount});
+    res.render("user", {username: req.session.username, viewCount: req.session.viewCount});
   } else {
     res.redirect("/");
   }
